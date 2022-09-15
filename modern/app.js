b5789e88d41781077198861f747090fd61530105
JFHarris <JFHarris1983@gmail.com>
2022-09-15 03:12:40 -0500

    EOD---Updated styles

--- modern/app.js
+++ modern/app.js
@@ -1,21 +1,11 @@
-document.addEventListener('DOMContentLoaded', nav)
-
-function nav(){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.main-nav');
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-    })
-}
-
 const scrollUp = document.querySelector("#scroll-up");
 
 scrollUp.addEventListener("click", () => {
-    window.scrollTo({
+  window.scrollTo({
     top: 0,
     left: 0,
     behavior: "smooth",
-    });
+  });
 });
 
 const domContainer = document.querySelector('#scroll-up');
